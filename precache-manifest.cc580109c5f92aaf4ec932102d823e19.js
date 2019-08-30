8ae1554e0729be312f48b421fded10d7743d40e1
Giancarlo Arias Silva <giancarloariasva@gmail.com>
2019-08-30 04:06:54 +0200

    Updates

---
+++ precache-manifest.cc580109c5f92aaf4ec932102d823e19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a9dd9650b904bbe1814d07c80ce542",
+    "url": "/gianprova/index.html"
+  },
+  {
+    "revision": "43e5e3c72e9224ab0878",
+    "url": "/gianprova/static/css/main.44ee8014.chunk.css"
+  },
+  {
+    "revision": "f7b4321ad7e92fc1adb4",
+    "url": "/gianprova/static/js/2.d0f56a5f.chunk.js"
+  },
+  {
+    "revision": "43e5e3c72e9224ab0878",
+    "url": "/gianprova/static/js/main.3bf7e629.chunk.js"
+  },
+  {
+    "revision": "8707fee7eeee606dea7d",
+    "url": "/gianprova/static/js/runtime~main.980b219b.js"
+  }
+]);
